ccbef3117a5004fffd5a5377333a75a64b0e4400
rahoc369782 <rahuldarekar273@gmail.com>
2021-07-01 12:02:32 +0530

    new set up

---
+++ src/components/WorkspaceSelection.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class WorkspaceSelection extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
